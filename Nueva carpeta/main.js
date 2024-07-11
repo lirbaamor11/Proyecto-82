42f7ae5c0d06499555c452ebc40f68badb6b4bbc
lirbaamor11 <namorg76@gmail.com>
2024-07-11 17:09:09 -0600

    Add files via upload

---
+++ Nueva carpeta/main.js
@@ -0,0 +1,31 @@
+console.log("Hola! soy la pantalla de la consola");
+
+var first_name = "Abril";
+            
+        console.log(fist_name)
+
+        var last_name = "Ramírez";
+        console.log(last_name);
+        
+        var full_name = first_name.concept(last_name)
+        console.log(full_name);
+        var lenght_oF_fullname = full_name.lenght;
+        console.log(lenght_oF_full_name);
+
+        var name_one= "Me llamo Abril Ramirez";
+        console.log(name_one);
+        var replace_name = name.one.replace("Ramírez","Gallardo")
+        console.log(replace_name);
+
+        var capital_name = "MIA";
+        var making_lower_case = capital_name.toLowerCase();
+        console.log(making_lower_case);
+
+        var square_root = Math.sqrt(16);
+        console.log(square_root);
+        var maximum_number = Math_max(15,10, 5, 20);
+        console.log(maximum_number);
+        var power_eg_1 = Math.pow(4,2);
+        console.log(power eg 1);
+
+        
